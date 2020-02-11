f43fb131006e8361b70e6c19e3566664212b8aa9
derek-cornelison <dcornelison1216@gmail.com>
2020-02-11 17:52:36 -0500

    4/26 tests pass

--- index.js
+++ index.js
@@ -87,8 +87,11 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  const reduceNumberList = numberList.reduce((total, num) => {
+    return total += num;
+  }, 0);
+  return callback(reduceNumberList);
 }
 
 /**
