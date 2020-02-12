d5dd083749cac17103e3b973eaf9a8a3a5012675
derek-cornelison <dcornelison1216@gmail.com>
2020-02-12 17:18:21 -0500

    7/26 tests pass

--- index.js
+++ index.js
@@ -162,8 +162,12 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  const newList = list.filter((item, index) => {
+    return list.indexOf(item) === index
+  });
+
+  return callback(newList);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
